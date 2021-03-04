1167806ad0d0f71afb5ef39410d8003c03648a32
Mudassir Sayed <mudassir@nimapinfotech.com>
2021-03-04 11:58:38 +0530

    Updates

---
+++ precache-manifest.d11014ec9b2464bcc51f599de16cc1d5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fba746f788e2a2c647619290c34339",
+    "url": "/react-responsive-website/index.html"
+  },
+  {
+    "revision": "d9a1a6a36ae24d7e24ca",
+    "url": "/react-responsive-website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ec2fb55da009b2e282c",
+    "url": "/react-responsive-website/static/js/2.9cf48b78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-responsive-website/static/js/2.9cf48b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a1a6a36ae24d7e24ca",
+    "url": "/react-responsive-website/static/js/main.5d2ee366.chunk.js"
+  },
+  {
+    "revision": "da3e764f13942fbfd059",
+    "url": "/react-responsive-website/static/js/runtime-main.45f6af08.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/react-responsive-website/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/react-responsive-website/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/react-responsive-website/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/react-responsive-website/static/media/svg-3.397c455a.svg"
+  }
+]);
